f03e4f3d9de862b2ee3abfd624d2ee88061917e9
Dries Vints <dries@vints.io>
2021-06-23 22:42:17 +0200

    last cleanup

--- src/Generation/IconGenerator.php
+++ src/Generation/IconGenerator.php
@@ -4,138 +4,70 @@ declare(strict_types=1);
 
 namespace BladeUI\Icons\Generation;
 
-use Closure;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\SingleCommandApplication;
-use Symfony\Component\Finder\SplFileInfo;
+use Illuminate\Support\Stringable;
 
 final class IconGenerator
 {
     private Filesystem $filesystem;
 
-    private string $library;
+    private array $sets;
 
-    private string $root;
-
-    private array $sets = [];
-
-    private ?Closure $svgNormalizationClosure = null;
-
-    private bool $useSingleIconSet = false;
-
-    private bool $deleteExistingIcons = true;
-
-    private function __construct(string $library)
+    public function __construct(array $sets)
     {
         $this->filesystem = new Filesystem();
-        $this->library = $library;
-        $this->root = getcwd();
-    }
-
-    public static function create(string $set): self
-    {
-        return new self($set);
-    }
-
-    public function withIconSets(array $sets): self
-    {
         $this->sets = $sets;
-
-        return $this;
     }
 
-    public function withSvgNormalisation(Closure $svgNormalizationClosure): self
+    public static function create(array $config): self
     {
-        $this->svgNormalizationClosure = $svgNormalizationClosure;
-
-        return $this;
+        return new self($config);
     }
 
-    public function useSingleIconSet(): self
+    public function generate(): void
     {
-        $this->useSingleIconSet = true;
+        foreach ($this->sets as $set) {
+            $destination = $this->getDestinationDirectory($set);
 
-        return $this;
-    }
+            foreach ($this->filesystem->files($set['source']) as $file) {
+                $filename = Str::of($file->getFilename());
+                $filename = $this->applyPrefixes($set, $filename);
+                $pathname = $destination . $filename;
 
-    public function preserveExistingIcons(): self
-    {
-        $this->deleteExistingIcons = false;
+                $this->filesystem->copy($file->getRealPath(), $pathname);
 
-        return $this;
-    }
-
-    public function run()
-    {
-        return (new SingleCommandApplication())
-            ->setCode(function (InputInterface $input, OutputInterface $output) {
-                $output->writeln("Starting to generate icons for the {$this->library} package...");
-
-                if ($this->deleteExistingIcons) {
-                    $this->filesystem->deleteDirectory($this->defaultDestination());
+                if (is_callable($set['after'] ?? null )) {
+                    $set['after']($pathname, $set);
                 }
-
-                $this->filesystem->ensureDirectoryExists($this->defaultDestination());
-
-                foreach ($this->sets as $config) {
-                    $iconFileList = $this->filesystem->files($config['source']);
-
-                    $this->generateIcons($config, $iconFileList);
-                }
-
-                $output->writeln("Finished generating icons for the {$this->library} package!");
-
-                return Command::SUCCESS;
-            })
-            ->run();
-    }
-
-    /**
-     * @param SplFileInfo[] $icons
-     */
-    public function generateIcons(array $config, array $icons): void
-    {
-        foreach ($icons as $file) {
-            $destination = Str::finish(
-                $config['destination'] ?? $this->defaultDestination(), DIRECTORY_SEPARATOR
-            );
-
-            // Concat the set name onto the path...
-            if (! $this->useSingleIconSet) {
-                $destination .= $this->library.DIRECTORY_SEPARATOR;
-            }
-
-            $this->filesystem->ensureDirectoryExists($destination);
-
-            $finalFile = Str::of($file->getFilename());
-
-            if ($config['input-prefix'] ?? false) {
-                $finalFile = $finalFile->after($config['input-prefix']);
-            }
-
-            if ($config['prefix'] ?? false) {
-                $finalFile = $finalFile->prepend($config['prefix']);
-            }
-
-            $finalFile = $destination.$finalFile;
-
-            // Copy to final destination...
-            copy($file->getRealPath(), $finalFile);
-
-            // Apply user transformations if they provide them...
-            if ($this->svgNormalizationClosure !== null) {
-                $normalizeSvgClosure = $this->svgNormalizationClosure;
-                $normalizeSvgClosure($finalFile, $config);
             }
         }
     }
 
-    private function defaultDestination(): string
+    private function getDestinationDirectory(array $set): string
     {
-        return $this->root.'/resources/svg';
+        $destination = Str::finish($set['destination'], DIRECTORY_SEPARATOR);
+
+        if (! Arr::get($set, 'safe', false)) {
+            $this->filesystem->deleteDirectory($destination);
+        }
+
+        $this->filesystem->ensureDirectoryExists($destination);
+
+        return $destination;
+    }
+
+    private function applyPrefixes($set, Stringable $filename): Stringable
+    {
+        if ($set['input-prefix'] ?? false) {
+            $filename = $filename->after($set['input-prefix']);
+        }
+
+        if ($set['output-prefix'] ?? false) {
+            $filename = $filename->prepend($set['output-prefix']);
+        }
+
+        return $filename;
     }
 }
