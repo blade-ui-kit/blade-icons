3622ef90ba6f56b28e1d07f6764e6a65b3f1e606
Dries Vints <dries@vints.be>
2023-06-09 17:21:34 +0200

    wip

--- tests/IconsManifestTest.php
+++ tests/IconsManifestTest.php
@@ -45,11 +45,11 @@ class IconsManifestTest extends TestCase
         $this->assertTrue(file_exists($this->manifestPath));
         $this->assertEquals(
             $this->expectedManifest(),
-            str_replace(
-                ["\n", '=> '.PHP_EOL, '\\'],
-                [PHP_EOL, '=>'.PHP_EOL, '\\\\'],
+            preg_replace("/\\\\/", "\\", str_replace(
+                ["\n", '=> '.PHP_EOL],
+                [PHP_EOL, '=>'.PHP_EOL],
                 file_get_contents($this->manifestPath)
-            ),
+            )),
         );
     }
 
