840f51ed7ab58b2b213b0aab543ae46a334caddb
driesvints <driesvints@users.noreply.github.com>
2023-06-09 15:36:15 +0000

    Fix code styling

--- tests/fixtures/blade-icons.php
+++ tests/fixtures/blade-icons.php
@@ -1,20 +1,18 @@
-<?php return array (
-  'default' => 
-  array (
-    '/Users/driesvints/Sites/blade-ui-kit/blade-icons/tests/resources/svg' => 
-    array (
-      0 => 'camera',
-      1 => 'foo-camera',
-      2 => 'solid.camera',
-      3 => 'xml',
-      4 => 'zondicon-flag',
-    ),
-  ),
-  'zondicons' => 
-  array (
-    '/Users/driesvints/Sites/blade-ui-kit/blade-icons/tests/resources/zondicons' => 
-    array (
-      0 => 'flag',
-    ),
-  ),
-);
+<?php
+
+return [
+    'default' => [
+        '/Users/driesvints/Sites/blade-ui-kit/blade-icons/tests/resources/svg' => [
+            0 => 'camera',
+            1 => 'foo-camera',
+            2 => 'solid.camera',
+            3 => 'xml',
+            4 => 'zondicon-flag',
+        ],
+    ],
+    'zondicons' => [
+        '/Users/driesvints/Sites/blade-ui-kit/blade-icons/tests/resources/zondicons' => [
+            0 => 'flag',
+        ],
+    ],
+];
